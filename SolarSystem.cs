00cef67b3c250207b06ab63dea7757c3b5bc0265
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2022-12-21 20:04:50 -0500

    Create SolarSystem.cs

---
+++ SolarSystem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SolarLibrary
+{
+  public class SolarSystem
+  {
+    public string name;
+    public string notes;
+    public Star[] stars = new Star[4];
+    public Planet[] planets = new Planet[99];
+  }
+}
