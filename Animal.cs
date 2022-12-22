dff2830f82ed004ccddac5481eebb0d0c4a6a528
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2022-12-21 20:00:20 -0500

    Create Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace SolarLibrary
+{
+  public class Animal
+  {
+    public string notes;
+		public string name;
+    public string appearance;
+  	public string height;
+  	public string lifespan;
+    public string maturity;
+  	public string breedingAge;
+ 	  public string young;
+    public string population;
+    public string temperament;
+  	public string diet;
+  	public string familyLife;
+    public string general;
+  }
+}
