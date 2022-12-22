c419b89b5517207e3dad7f38ff23985fc852a1a3
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2022-12-21 20:03:39 -0500

    Create Star.cs

---
+++ Star.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Solar-System-Lib
+{
+  public class Star
+  {
+    public string name;
+    public string type;
+    public double age; // age of the star in myo
+    public double size; //in solar mass
+    public double radius; // x the suns radius
+    public double habZoneIn;//in au
+    public double habZoneOut;
+    public double habZoneWidth;
+  }
+}
